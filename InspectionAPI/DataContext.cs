0ff4fea17840267cffd39873122131a4f893b42c
maalloor <maalloor@espol.edu.ec>
2022-02-05 18:48:47 -0500

    API used for inspection management

---
+++ InspectionAPI/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionType { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
